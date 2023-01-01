27809dffdc3db7dfb76bf83d33484cce459ce37a
amirhrb <amirrabi70@gmail.com>
2023-01-01 16:06:06 +0330

    necessary changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,121 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { useEffect, useRef, useState } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function ScrollbarX({
+  w = 180,
+  h = "6px",
+  r = "0px",
+  thumbColor = "#555",
+  trackColor = "#cecece",
+}) {
+  const barRef = useRef();
+  const [{ scrolledRatio, thumbOnTrack }, setScroll] = useState({
+    thumbOnTrack: 0,
+    scrolledRatio: 0,
+  });
+  useEffect(() => {
+    setScroll({
+      thumbOnTrack: Number(
+        barRef.current.previousSibling.clientWidth /
+          barRef.current.previousSibling.scrollWidth
+      ).toFixed(2),
+      scrolledRatio: +(
+        -barRef.current.scrollLeft /
+        (barRef.current.scrollWidth - barRef.current.clientWidth)
+      ).toFixed(2),
+    });
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    barRef.current.previousSibling.addEventListener("scroll", (e) => {
+      setScroll({
+        thumbOnTrack: Number(
+          e.target.clientWidth / e.target.scrollWidth
+        ).toFixed(2),
+        scrolledRatio: +(
+          -e.target.scrollLeft /
+          (e.target.scrollWidth - e.target.clientWidth)
+        ).toFixed(2),
+      });
+    });
+  }, []);
+  return (
+    <div
+      ref={barRef}
+      style={{
+        width: w,
+        height: h,
+        borderRadius: r,
+        backgroundColor: trackColor,
+      }}
+    >
+      <div
+        style={{
+          width: w * thumbOnTrack,
+          position: "relative",
+          right: w * (1 - thumbOnTrack) * scrolledRatio,
+          height: h,
+          borderRadius: r,
+          backgroundColor: thumbColor,
+        }}
+      ></div>
+    </div>
+  );
+}
+function ScrollbarY({
+  h = 180,
+  w = "6px",
+  r = "0px",
+  thumbColor = "#555",
+  trackColor = "#cecece",
+}) {
+  const barRef = useRef();
+  const [{ scrolledRatio, thumbOnTrack }, setScroll] = useState({
+    thumbOnTrack: 0,
+    scrolledRatio: 0,
+  });
+  useEffect(() => {
+    setScroll({
+      thumbOnTrack: Number(
+        barRef.current.previousSibling.clientHeight /
+          barRef.current.previousSibling.scrollHeight
+      ).toFixed(2),
+      scrolledRatio: +(
+        -barRef.current.scrollTop /
+        (barRef.current.scrollHeight - barRef.current.clientHeight)
+      ).toFixed(2),
+    });
+
+    barRef.current.previousSibling.addEventListener("scroll", (e) => {
+      setScroll({
+        thumbOnTrack: Number(
+          e.target.clientHeight / e.target.scrollHeight
+        ).toFixed(2),
+        scrolledRatio: +(
+          -e.target.scrollTop /
+          (e.target.scrollHeight - e.target.clientHeight)
+        ).toFixed(2),
+      });
+    });
+  }, []);
+  return (
+    <div
+      ref={barRef}
+      style={{
+        width: w,
+        height: h,
+        borderRadius: r,
+        backgroundColor: trackColor,
+      }}
+    >
+      <div
+        style={{
+          height: h * thumbOnTrack,
+          position: "relative",
+          top: h * (1 - thumbOnTrack) * scrolledRatio,
+          width: w,
+          borderRadius: r,
+          backgroundColor: thumbColor,
+        }}
+      ></div>
+    </div>
+  );
+}
+export { ScrollbarX, ScrollbarY };
